d526d6ba22fd261bac87f300150233b3443bbe32
Eman Butt <butt.eman391@gmail.com>
2025-05-10 01:47:05 +0500

    Added dynamic pages.

--- Assets/scripts/memory-jar.js
+++ Assets/scripts/memory-jar.js
@@ -1,5 +1,17 @@
 let currentPage = 0;
 
+$(function(){
+   let saved_book = localStorage.getItem("book_saved");
+    if (saved_book){
+        $(".book").html(saved_book);
+
+        $('.page').each(function(index) {
+            const total = $('.page').length;
+            $(this).css('z-index', total - index);
+        });
+    }
+});
+
 function flipNext() {
   const pages = document.querySelectorAll('.page');
   if (currentPage < pages.length) {
@@ -24,7 +36,31 @@ $(function(){
 
 
 $(function(){
+    $("#add-memory-page").hide();
     $("#add-memory").on("click", function(){
+        $("#add-memory-page").toggle();
+    });
 
+    $("#add").on("click", function(){
+        let title = $("#title").val();
+        let memory = $("#memory").val();
+    
+        let book = $(".book");
+
+        let page = `<div class="page">
+                    <div class="memory-title">
+                    ${title}</div>
+                    <div class="memory-desc">
+                    ${memory}</div>
+                    </div>`;
+
+        book.append(page);
+        $("#title").val("");
+        $("#memory").val("");
+
+        localStorage.setItem("book_saved", book.html());
+
+        $("#add-memory-page").hide();
+        
     });
 });
